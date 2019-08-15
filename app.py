e3de2875001447112da08fe781b43ab5fb7bc1c1
Chris Martin <chris@sensedev.com>
2019-08-15 15:51:55 -0700

    afternoon commit

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template, request, redirect
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "survey"
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# app.config["SECRET_KEY"] = "survey"
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 
 responses = []
-
-quest = 0
+question_number = 0
 
 @app.route("/")
 def survey():
     """Showing survey form."""
-
     return render_template("survey.html", title=satisfaction_survey.title,
         instructions=satisfaction_survey.instructions)
 
@@ -25,16 +23,25 @@ def survey():
 def question(quest):
     """Showing one question from survey."""
     # if(quest == len(satisfaction_survey.questions)):
-
- 
     
     return render_template("question.html", question=satisfaction_survey.questions[quest].question, choices=satisfaction_survey.questions[quest].choices, quest=quest)
 
+
 @app.route("/answers", methods=["POST"])
-def answers(quest):
+def answers():
     """Receiving answers."""
     
     responses.append(request.form["choice_made"])
-    quest += 1
-    return redirect(f"/questions/{quest}")
+    quest = int(request.form["question_number"])
 
+    if quest == len(satisfaction_survey.questions) - 1:
+        return redirect('/thankyou')
+    else:
+        quest += 1
+        return redirect(f"/questions/{quest}")
+
+@app.route('/thankyou')
+def thank_you():
+    """Render thank you page"""
+
+    return render_template('thankyou.html')
