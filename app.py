df404d9e1db06d6e680dfb6559b28f86a01c700f
jenniferhm <48392176+jenniferhm@users.noreply.github.com>
2019-08-15 17:24:47 -0700

    third commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -9,13 +9,12 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 question_number = 0
 
 @app.route("/")
 def survey():
     """Showing survey form."""
-    responses = []
+
     return render_template("survey.html", title=satisfaction_survey.title,
         instructions=satisfaction_survey.instructions)
 
@@ -26,9 +25,9 @@ def question(quest):
     # if(quest == len(satisfaction_survey.questions)):
 
     """Check if on the right question"""
-    if len(responses) != quest:
+    if int(len(session["responses"])) != int(quest):
         flash("Please answer")
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(session['responses'])}")
     else:
         return render_template("question.html", question=satisfaction_survey.questions[quest].question, choices=satisfaction_survey.questions[quest].choices, quest=quest)
 
@@ -36,10 +35,12 @@ def question(quest):
 @app.route("/answers", methods=["POST"])
 def answers():
     """Receiving answers."""
-    
-    responses.append(request.form["choice_made"])
+    current = session["responses"]
+    current.append(request.form["choice_made"])
+    session["responses"] = current
+
     quest = int(request.form["question_number"])
-    
+
 
     """Check if limit of questions has been reached"""
     if quest == len(satisfaction_survey.questions) - 1:
@@ -48,8 +49,18 @@ def answers():
         quest += 1
         return redirect(f"/questions/{quest}")
 
+
 @app.route('/thankyou')
 def thank_you():
     """Render thank you page"""
 
     return render_template('thankyou.html')
+
+
+@app.route("/setup", methods=["POST"])
+def setup():
+    """Creating our session storage"""
+
+    session["responses"] = []
+    
+    return redirect("/questions/0")
